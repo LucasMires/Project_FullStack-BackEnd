ac3454dc511f1f6724b40aceec124193222cfa9e
Lucas Mires <lucasmires10@gmail.com>
2023-06-05 06:29:59 -0300

    feat: enable Cors in application

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,34 @@ import { DocumentBuilder } from '@nestjs/swagger'
 import { SwaggerModule } from '@nestjs/swagger/dist'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create(AppModule)
+    app.enableCors()
 
-  const config = new DocumentBuilder()
-  .setTitle("SV Contacts")
-  .setDescription("Simple way to save and view your contacts")
-  .setVersion("0.5.0")
-  .addBearerAuth()
-  .build()
+    const config = new DocumentBuilder()
+    .setTitle("SV Contacts")
+    .setDescription("Simple way to save and view your contacts")
+    .setVersion("0.5.0")
+    .addBearerAuth()
+    .build()
 
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup("api/docs", app, document)
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("api/docs", app, document)
 
-  const prismaService = app.get(PrismaService)
-  await prismaService.enableShutDownHooks(app) 
+    const prismaService = app.get(PrismaService)
+    await prismaService.enableShutDownHooks(app) 
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      whitelist: true
-    }),
-    new ValidationPipe({
-      transform: true,
-      transformOptions: {
-        groups:["transform"]
-      }
-    })
-  )
+    app.useGlobalPipes(
+        new ValidationPipe({
+            whitelist: true
+        }),
+        new ValidationPipe({
+            transform: true,
+            transformOptions: {
+                groups:["transform"]
+            }
+        })
+    )
 
-  await app.listen(3000)
+    await app.listen(3000)
 }
 bootstrap()
